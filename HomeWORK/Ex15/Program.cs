0b2419557df07f280e0018fefc4cbb58d2be3873
darlin_alderson <dash.cobain@gmail.com>
2022-12-09 22:29:00 +0300

    ex15

---
+++ HomeWORK/Ex15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+{
+Console.WriteLine("(Это выходной день) -> да");
+}
+  else if (dayNumber < 1 || dayNumber > 7) 
+{
+Console.WriteLine("Это вообще не день недели");
+}
+  else Console.WriteLine("(Это выходной?) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
