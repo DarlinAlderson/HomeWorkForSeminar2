c2a33a9ab3624343fe935e2d2f58df5f4919edf1
darlin_alderson <dash.cobain@gmail.com>
2022-12-09 22:01:09 +0300

    ex10

---
+++ Ex001/Program.cs
@@ -0,0 +1,6 @@
+﻿int i = 0;
+while (i < 10)
+{
+    System.Console.WriteLine(i);
+i++;
+}
---
+++ HomeWORK/Ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("end"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
+
+
